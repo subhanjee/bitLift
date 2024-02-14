7283d49f0e9bd8c00de0c267884e9367105453ce
subhanjee <subanjee14@gmail.com>
2024-02-14 18:23:15 +0500

    done

--- app/components/participate/index.js
+++ app/components/participate/index.js
@@ -9,9 +9,9 @@ function Participate() {
     <div className="flex justify-center items-center flex-col paddPart " >
       <Image src={hero} alt="" className="hero-ab w-full "/>
       <h1 className="text-center radial-text-gradient-01 font-size-part relative  ">
-        How to participate?
+        How to participate? 
       </h1>  
-      <div className="flex justify-evenly items-center mt-10 w-[25rem] md:w-[33rem] relative">
+      <div className="flex justify-evenly items-center mt-10 w-[22rem] md:w-[33rem] relative">
         <button className="px-6 py-2 text-white border  rounded-full hover:border part-font ">
           Participate
         </button>
